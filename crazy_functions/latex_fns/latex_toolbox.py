1e48ce0381df338b160a3894dc1bd7770a5c77ae
kelvin34501 <kelvin34501@gmail.com>
2024-10-06 13:31:10 +0800

    mod: main vote tweak

--- crazy_functions/latex_fns/latex_toolbox.py
+++ crazy_functions/latex_fns/latex_toolbox.py
@@ -316,8 +316,15 @@ def find_main_tex_file(file_manifest, mode):
             "reviewers",
         ]
         expected_words = ["\\input", "\\ref", "\\cite"]
-        for texf in candidates:
+        for texf in canidates:
             candidates_score.append(0)
+            
+            last_part = os.path.basename(texf)
+            if "supp" in last_part or "appendix" in last_part:
+                candidates_score[-1] -= 5
+            if "main" in last_part:
+                candidates_score[-1] += 4
+
             with open(texf, "r", encoding="utf8", errors="ignore") as f:
                 file_content = f.read()
                 file_content = rm_comments(file_content)
